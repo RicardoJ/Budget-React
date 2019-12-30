511210d7192cdac61d5d70017f1436797459543c
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-12-30 16:09:38 -0500

    validation budget

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function Question() {
+
+    const [amount , saveAmount] = useState(0);
+    const [Error , saveError] = useState(false);
+
+    const addBudget = e =>{
+        e.preventDefault();
+
+        if (amount <= 0 || isNaN(amount)) {
+            saveError(true);
+            return;
+        }
+    }
     return (
         <Fragment>
             <h2>Tu Presupuesto</h2>
-            <form>
+            <form
+            onSubmit = {addBudget}
+            >
                 <input
                 type = "number"
                 className = "u-full-width"
                 placeholder = "Agrega tu presupuesto"
-                //onChange = {}
+                onChange = {e => saveAmount(parseInt( e.target.value , 10))}
                 />
                 <input type = "submit"
                 className = "button-primary u-full-width"
